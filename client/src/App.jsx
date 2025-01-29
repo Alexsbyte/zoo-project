6a661d91c254180c46bfdcbc419c12b6adf53ea2
Sensdoo <sensdoo.dev@gmail.com>
2025-01-29 16:43:26 +0300

    done server side reg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,3 +38,5 @@ function App() {
 
   return <RouterProvider router={router}/>
 }
+
+export default App
--- server/src/config/serverConfig.js
+++ server/src/config/serverConfig.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require('express');
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const verifyAccessToken = require('../middleware/verifyAccesToken')
-const removeHeader = require('../middleware/removeHeader')
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const verifyAccessToken = require('../middleware/verifyAccesToken');
+const removeHeader = require('../middleware/removeHeader');
 
-const serverConfig = (app) =>{
-    app.use(cookieParser())
-    app.use(morgan('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cors({ credentials: true , origin: ['http://localhost:5173']}));
-    app.use(verifyAccessToken)
-    app.use(removeHeader);
-}
+const serverConfig = (app) => {
+  app.use(cookieParser());
+  app.use(morgan('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors({ credentials: true, origin: ['http://localhost:5173'] }));
+  // app.use(verifyAccessToken)
+  app.use(removeHeader);
+};
 
-module.exports = serverConfig
+module.exports = serverConfig;
