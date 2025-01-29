781096d47c6e03767bae374b11dd42016d985b3a
Никита Матигулин <nikitamatigulin@MacBook-Pro-Nikita.local>
2025-01-29 17:11:29 +0300

    setup-price-pages-are-ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,49 @@
-import { useState , useEffect} from 'react'
-import Layout from './pages/Layout'
-import MainPage from './pages/MainPage/MainPage'
-import RegPage from './pages/RegPage/RegPage'
-import LoginPage from './pages/LoginPage/LoginPage'
-import 'bulma/css/bulma.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import AnimalsPage from './pages/AnimalsPage/AnimalsPage'
-import AdminPage from './pages/AdminPage/AdminPage'
-
-
+import { useState, useEffect } from "react";
+import Layout from "./pages/Layout";
+import MainPage from "./pages/MainPage/MainPage";
+import RegPage from "./pages/RegPage/RegPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import AnimalsPage from "./pages/AnimalsPage/AnimalsPage";
+import AdminPage from "./pages/AdminPage/AdminPage";
+import TariffsPage from "./pages/TariffsPage/TariffsPage";
+import "bulma/css/bulma.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
       {
-        path: '/',
-        element: <MainPage />
+        path: "/",
+        element: <MainPage />,
       },
       {
-        path: '/auth/reg',
-        element: <RegPage  />
-      }
-      ,
-      {
-        path: '/auth/login',
-        element: <LoginPage />
+        path: "/auth/reg",
+        element: <RegPage />,
       },
       {
-        path: '/animals',
-        element: <AnimalsPage />
+        path: "/auth/login",
+        element: <LoginPage />,
       },
       {
-        path: '/admin',
-        element: <AdminPage />
-      }
-    ]
-  }
-])
-
+        path: "/animals",
+        element: <AnimalsPage />,
+      },
+      {
+        path: "/admin",
+        element: <AdminPage />,
+      },
+      {
+        path: "/tariffs",
+        element: <TariffsPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Nav from '../shared/ui/Nav'
-import Footer from '../shared/ui/Footer'
-import {Outlet} from 'react-router-dom'
+import React from "react";
+import Nav from "../shared/ui/Nav";
+import Footer from "../shared/ui/Footer";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-   <>
-   <Nav />
-   <Outlet />
-   <Footer />
-   </>
-  )
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <Nav />
+      <main style={{ flex: "1" }}>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
