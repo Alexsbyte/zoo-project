d7127307bade2d8ffa191ed943650b882643194a
Alexsbyte <kemper33157@gmail.com>
2025-01-29 13:45:13 +0300

    refreshTokens hand

--- server/src/utils/generateTokens.js
+++ server/src/utils/generateTokens.js
@@ -4,7 +4,7 @@ const jwtConfig = require('../config/jwtConfig');
 
 const generateTokens = (payload) => ({
     accessToken: jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: jwtConfig.access.expiresIn }),
-    // refreshToken: jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: jwtConfig.refresh.expiresIn }),
+    refreshToken: jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: jwtConfig.refresh.expiresIn }),
 });
 
 module.exports = { generateTokens };
