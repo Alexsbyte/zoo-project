59977496331507bf689c69386aa8c95a06fa94ca
Sensdoo <sensdoo.dev@gmail.com>
2025-01-30 11:25:28 +0300

    added try-catch to logout

--- server/src/routes/auth/logout.routes.js
+++ server/src/routes/auth/logout.routes.js
@@ -4,9 +4,13 @@ const formatResponse = require('../../utils/formatResponse');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res
-    .clearCookie(jwtConfig.refresh.clearCookie)
-    .json(formatResponse('200', 'logout success'));
+  try {
+    res
+      .clearCookie(jwtConfig.refresh.clearCookie)
+      .json(formatResponse('200', 'logout success'));
+  } catch (error) {
+    res.json(formatResponse('500', 'Server error', null, error.message));
+  }
 });
 
 module.exports = router;
